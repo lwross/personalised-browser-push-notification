ed5723c43719982e64c5f977e45e92e1f036d65d
Lachlan Ross <lachlan.ross@salesforce.com>
2017-01-25 18:55:41 +1100

    no message

--- public/ixn/activities/hello-world/js/HelloWorld.js
+++ public/ixn/activities/hello-world/js/HelloWorld.js
@@ -1,90 +1,175 @@
 define( function( require ) {
+
+    'use strict';
+    
 	var Postmonger = require( 'postmonger' );
 	var $ = require( 'vendor/jquery.min' );
 
     var connection = new Postmonger.Session();
-	var tokens;
+    var toJbPayload = {};
+    var step = 1; 
 	var endpoints;
-	var inArgPayload = {};
-
-    $(window).ready(function() {
-        connection.trigger('ready');
-		connection.trigger('requestTokens');
-		connection.trigger('requestEndpoints');
-    });
-
-	// This listens for Journey Builder to send tokens
-	// Parameter is either the tokens data or an object with an
-	// "error" property containing the error message
-	connection.on('getTokens', function( data ) {
-		if( data.error ) {
-			console.error( data.error );
-		} else {
-			tokens = data;
-		}
-	});
 	
-	connection.on('initActivity', function(payload) {
+    $(window).ready(onRender);
+
+    connection.on('initActivity', function(payload) {
+        var amount;
+
         if (payload) {
-        	inArgPayload = payload;
-            console.log('payload',JSON.stringify(payload));			
+            toJbPayload = payload;
+            console.log('payload',payload);
+            
+			//merge the array of objects.
+			var aArgs = toJbPayload['arguments'].execute.inArguments;
+			var oArgs = {};
+			for (var i=0; i<aArgs.length; i++) {  
+				for (var key in aArgs[i]) { 
+					oArgs[key] = aArgs[i][key]; 
+				}
+			}
+			//oArgs.amount will contain a value if this activity has already been configured:
+			amount = oArgs.amount || toJbPayload['configurationArguments'].defaults.amount;            
         }
+        
+		$.get( "/version", function( data ) {
+			$('#version').html('Version: ' + data.version);
+		});                
+
+        // If there is no amount selected, disable the next button
+        if (!amount) {
+            connection.trigger('updateButton', { button: 'next', enabled: false });
+        }
+
+		$('#selectAmount').find('option[value='+ amount +']').attr('selected', 'selected');		
+		gotoStep(step);
         
     });
 
-	/**
-		If you want to have a multi-step configuration view, you need to manage the DOM manually.
-		You can filter what changes to make by implementing the following type of logic when Postmonger from the server triggers an "updateStep" call.
-		// connection.on('updateStep', step ) {
-
-			if( step  >= 1 && step <= 3 ) {
-				$('.step').hide(); // All DOM elements which are steps should have this class (this hides them all)
-				$('#step' + step ).show(); // This selectively only displays the current step
-				// Allow the user to make any changes and when you're ready, use:
-				connection.trigger( 'updateStep', step ); 
-			}
-		}
-	**/
-	connection.on('updateStep', function( data ) {
-		inArgPayload['arguments'].execute.inArguments.push({"message": $('#message').val().trim()});
-		inArgPayload.metaData.isConfigured = true;
-        connection.trigger('updateActivity',inArgPayload);
-		
-	});
-	// This listens for Journey Builder to send endpoints
-	// Parameter is either the endpoints data or an object with an
-	// "error" property containing the error message
-	connection.on('getEndpoints', function( data ) {
+    connection.on('requestedEndpoints', function(data) {
 		if( data.error ) {
 			console.error( data.error );
 		} else {
 			endpoints = data;
-		}
-	});
-
-    connection.on('requestPayload', function() {
-	 var payload = {};
- 
-        payload.options = {
-           
-        };
-
-		//TODO: Shouldn't this come from the data?
-        payload.flowDisplayName = 'HTTP Activity';
- 
-        connection.trigger('getPayload', payload);
+            console.log("endpoints",data);
+		}        
     });
 
-	// Journey Builder broadcasts this event to us after this module
-	// sends the "ready" method. JB parses the serialized object which
-	// consists of the Event Data and passes it to the
-	// "config.js.save.uri" as a POST
-    connection.on('populateFields', function(payload) {
-    });
-	
-	connection.on('clickedNext', function() {
-		inArgPayload.metaData.isConfigured = true;
-        connection.trigger('updateActivity',inArgPayload);
+    connection.on('clickedNext', function() {
+        step++;
+        gotoStep(step);
+        connection.trigger('ready');
     });
 
+    connection.on('clickedBack', function() {
+        step--;
+        gotoStep(step);
+        connection.trigger('ready');
+    });
+
+    function onRender() {
+        connection.trigger('ready');
+        connection.trigger('requestEndpoints');
+
+        // Disable the next button if a value isn't selected
+        $('#selectValueTier').change(function() {
+            var valueTier = getValueTier();
+            var type = getType();
+            var bonus = getBonus();
+            var valid = Boolean(valueTier) && Boolean(bonus) && Boolean(type);
+            connection.trigger('updateButton', { button: 'next', enabled: valid });
+        });
+
+        $('#selectType').change(function() {
+            var valueTier = getValueTier();
+            var type = getType();
+            var bonus = getBonus();
+            var valid = Boolean(valueTier) && Boolean(bonus) && Boolean(type);
+            connection.trigger('updateButton', { button: 'next', enabled: valid });
+        });
+
+        $('#selectBonus').change(function() {
+            var valueTier = getValueTier();
+            var type = getType();
+            var bonus = getBonus();
+            var valid = Boolean(valueTier) && Boolean(bonus) && Boolean(type);
+            connection.trigger('updateButton', { button: 'next', enabled: valid });
+        });
+
+
+    };
+
+    function gotoStep(step) {
+        $('.step').hide();
+        switch(step) {
+            case 1:
+                $('#step1').show();
+                var valueTier = getValueTier();
+                var type = getType();
+                var bonus = getBonus();
+                var valid = Boolean(valueTier) && Boolean(bonus) && Boolean(type);
+                connection.trigger('updateButton', { button: 'next', text: 'next', enabled: valid });
+                connection.trigger('updateButton', { button: 'back', visible: false });
+                break;
+            case 2:
+                $('#step2').show();
+                $('#showValueTier').html(getValueTierText());
+                $('#showType').html(getTypeText());
+                $('#showBonus').html(getBonusText());
+                connection.trigger('updateButton', { button: 'back', visible: true });
+                connection.trigger('updateButton', { button: 'next', text: 'done', visible: true });
+                break;
+            case 3: // Only 2 steps, so the equivalent of 'done' - send off the payload
+                save();
+                break;
+        }
+    };
+
+    function getValueTier() {
+        return $('#selectValueTier').find('option:selected').attr('value').trim();
+    };
+    function getType() {
+        return $('#selectType').find('option:selected').attr('value').trim();
+    };
+    function getBonus() {
+        return $('#selectBonus').find('option:selected').attr('value').trim();
+    };
+
+    function getValueTierText() {
+        return $('#selectValueTier').find('option:selected').text().trim();
+    };
+    function getTypeText() {
+        return $('#selectType').find('option:selected').text().trim();
+    };
+    function getBonusText() {
+        return $('#selectBonus').find('option:selected').text().trim();
+    };
+
+    function save() {
+
+        var valueTier = getValueTier();
+        var type = getType();
+        var bonus = getBonus();
+
+        // toJbPayload is initialized on populateFields above.  Journey Builder sends an initial payload with defaults
+        // set by this activity's config.json file.  Any property may be overridden as desired.
+        //toJbPayload.name = "my activity";
+
+        //this will be sent into the custom activity body within the inArguments array.
+        toJbPayload['arguments'].execute.inArguments.push({"valueTier": valueTier});
+        toJbPayload['arguments'].execute.inArguments.push({"type": type});
+        toJbPayload['arguments'].execute.inArguments.push({"bonus": bonus});
+
+		/*
+        toJbPayload['metaData'].things = 'stuff';
+        toJbPayload['metaData'].icon = 'path/to/icon/set/from/iframe/icon.png';
+        toJbPayload['configurationArguments'].version = '1.1'; // optional - for 3rd party to track their customActivity.js version
+        toJbPayload['configurationArguments'].partnerActivityId = '49198498';
+        toJbPayload['configurationArguments'].myConfiguration = 'configuration coming from iframe';
+		*/
+		
+		toJbPayload.metaData.isConfigured = true;  //this is required by JB to set the activity as Configured.
+        connection.trigger('updateActivity', toJbPayload);
+    }; 
+    	 
 });
+			
