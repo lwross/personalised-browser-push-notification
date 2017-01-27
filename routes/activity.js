d74d77bde671c5fef50e90f7deda7e1fdc52a907
Lachlan Ross <lachlan.ross@salesforce.com>
2017-01-27 11:30:19 +1100

    no message

--- routes/activity.js
+++ routes/activity.js
@@ -129,10 +129,38 @@ function isMC_API(url) {
  * POST Handler for /execute/ route of Activity.
  */
 exports.execute = function( req, res ) {
-	//console.log('body',util.inspect(req.body, {showHidden: false, depth: null}));
-	//console.log('body',JSON.stringify(req.body));
+	console.log('body',util.inspect(req.body, {showHidden: false, depth: null}));
+	console.log('body',JSON.stringify(req.body));
+	
 	var vapidKeys = webpush.generateVAPIDKeys();
+	console.log(vapidKeys);
+
+	webpush.setGCMAPIKey('AAAAEzBWVWU:APA91bH8wdRRiOtqdSAfLjrXlb1jmuvr_UExaeI9QDdjdiop_nSPHXUX0cfM-khb1qcm8V9uV61BLhibMxjkgeOHLtKMp2Z7zG8PMLO4iBZVx2SQ8jAKVia20RSL4nPEdhHZMkKbDrCYkxRJG5vX5T8DQPzFEunLXg');
+webpush.setVapidDetails(
+  'mailto:lachlan.ross@salesforce.com',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+
 console.log(vapidKeys);
+
+// This is the same output of calling JSON.stringify on a PushSubscription
+const pushSubscription = {
+  endpoint: 'https://android.googleapis.com/gcm/send/fzEIfdnQrfU:APA91bH0lc8utkMIS23Bi1Ub2VtIJiC1gFyOrHlQtx0BbmNL5mB1wbeX3L3y7bBoinvZqNsMS4lsZfiAV6CvgjL-BnYxsn-4Fby2UtM-pTH0BhPPwNjUMZnt_iVSzR97U96hcBRLzSy5',
+  keys: {
+    auth: 'K1kWulifseUljBmEnK5Qjg==',
+    p256dh: 'BJc5TsWIuOAq3OCd0am9apYU2hsSFrYsc6l-f6OxjjsFHQ8CuacD7uOaC98HC5YJJif40UGNg9xeLO3FdIeNDnk='
+  }
+};
+
+console.log(pushSubscription);
+
+webpush.sendNotification(pushSubscription, JSON.stringify({
+	  title: 'Hello World Title',
+      body: 'Hello World Body',
+      icon: '/images/icon-192x192.png',
+      tag: 'personalised-push-demo-notification-tag'
+    }));
 
 	/*
 	tests:
@@ -175,9 +203,9 @@ console.log(vapidKeys);
 		}
 	}	
 
-	//console.log('oArgs',util.inspect(oArgs, {showHidden: false, depth: null}));
-	//console.log('oArgs',JSON.stringify(oArgs));
-	//console.log('token',req.session.token);
+	console.log('oArgs',util.inspect(oArgs, {showHidden: false, depth: null}));
+	console.log('oArgs',JSON.stringify(oArgs));
+	console.log('token',req.session.token);
 	
 	var CLIENT_ID = 'myclientid';
 	var CLIENT_SECRET = 'myclientsecret';
